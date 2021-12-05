b19236ce3379f81ba68d8261b264faa00ef00493
lmperez <luismanuelp1992@gmail.com>
2021-12-04 21:31:18 -0500

    feat: sidebar admin menu

---
+++ src/app/admin/sidebar/menu-items/menu-items.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { INavItem } from '../menu-list-item/nav-item';
+
+const MENUITEMS: INavItem[] = [
+  {
+    displayName: 'Users',
+    iconName: 'people',
+    route: 'users',
+  },
+  {
+    displayName: 'Roles',
+    iconName: 'admin_panel_settings',
+    route: 'roles',
+  },
+  {
+    displayName: 'Products',
+    iconName: 'production_quantity_limits',
+    route: 'products',
+  },
+];
+
+@Injectable()
+export class MenuItems {
+  getMenuitem(): INavItem[] {
+    return MENUITEMS;
+  }
+}
---
+++ src/app/admin/sidebar/menu-list-item/nav-item.ts
@@ -0,0 +1,7 @@
+export interface INavItem {
+  displayName: string;
+  disabled?: boolean;
+  iconName?: string;
+  route?: string;
+  children?: INavItem[];
+}
