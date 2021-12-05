3be2d75c6b9789f747990aa48bc410c113284676
lmperez <luismanuelp1992@gmail.com>
2021-12-04 21:31:00 -0500

    ref: admin scaffold

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { MatDrawer, MatSidenav } from '@angular/material/sidenav';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
-export class AdminComponent implements OnInit {
+export class AdminComponent implements OnDestroy {
+  mobileQuery: MediaQueryList;
+  @ViewChild('drawer', { static: true }) drawer: MatDrawer | undefined ;
 
-  constructor() { }
+  constructor(
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher,
+  ) {
+    this.mobileQuery = media.matchMedia('(min-width: 768px)');
+    this.mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addEventListener('change', this.mobileQueryListener);
+  }
 
-  ngOnInit(): void {
+  private readonly mobileQueryListener: () => void;
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeEventListener('change', this.mobileQueryListener);
+  }
+
+  selectedDrawer(value: boolean) {
+    this.drawer?.toggle(value);
   }
 
 }
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,17 +1,30 @@
+import { NavService } from './sidebar/menu-list-item/nav.service';
+import { MenuItems } from './sidebar/menu-items/menu-items';
+import { MatIconModule } from '@angular/material/icon';
+import { MenuListItemComponent } from './sidebar/menu-list-item/menu-list-item.component';
+import { AppSidebarComponent } from './sidebar/sidebar.component';
 import { HeaderModule } from './../config/components/header/header.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    AppSidebarComponent,
+    MenuListItemComponent
   ],
   imports: [
     CommonModule,
     AdminRoutingModule,
-    HeaderModule
-  ]
+    HeaderModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule
+  ],
+  providers: [MenuItems, NavService]
 })
 export class AdminModule { }
