af7b6d0c63ae909fffc8d924a7950b4dd31da36e
lmperez <luismanuelp1992@gmail.com>
2021-12-08 16:21:38 -0500

    feat: list products

---
+++ src/app/admin/products/list/list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ListService } from './list.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent {
+
+  constructor(public listService: ListService) { }
+
+}
---
+++ src/app/admin/products/list/list.module.ts
@@ -0,0 +1,28 @@
+import { MatButtonModule } from '@angular/material/button';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListRoutingModule } from './list-routing.module';
+import { ListComponent } from './list.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { DataGridModule } from '../../../config/components/data-grid/data-grid.module';
+
+@NgModule({
+  declarations: [
+    ListComponent
+  ],
+  imports: [
+    CommonModule,
+    ListRoutingModule,
+    MatButtonModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatCardModule,
+    DataGridModule,
+    MatTableModule,
+    MatIconModule
+  ]
+})
+export class ListModule { }
