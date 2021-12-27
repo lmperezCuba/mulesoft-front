42852cdc0f3d33a3e56ad3de851225fb78ef4d61
lmperez <luismanuelp1992@gmail.com>
2021-12-27 14:58:47 -0500

    fix: auth guard on public

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -1,6 +1,7 @@
 import { PublicComponent } from './public.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../config/guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -9,15 +10,27 @@ const routes: Routes = [
     children: [
       {
         path: 'shop',
-        loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule)
+        loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule),
+        canActivate: [AuthGuard],
+        data: {
+          expectedRoles: ['USER']
+        }
       },
       {
         path: 'checkout',
-        loadChildren: () => import('./checkout/checkout.module').then(m => m.CheckoutModule)
+        loadChildren: () => import('./checkout/checkout.module').then(m => m.CheckoutModule),
+        canActivate: [AuthGuard],
+        data: {
+          expectedRoles: ['USER']
+        }
       },
       {
         path: 'shopping-list',
-        loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.ShoppingListModule)
+        loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.ShoppingListModule),
+        canActivate: [AuthGuard],
+        data: {
+          expectedRoles: ['USER']
+        }
       },
       {
         path: '',
